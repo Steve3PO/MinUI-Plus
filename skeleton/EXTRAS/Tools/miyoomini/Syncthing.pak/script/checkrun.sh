84fcce1e9c8cb5990949923dcba55348902ce30d
Steve3PO <stephenfalcone3@gmail.com>
2024-05-14 11:37:37 -0400

    remove old stuff/cleanup

--- skeleton/EXTRAS/Tools/miyoomini/Syncthing.pak/script/checkrun.sh
+++ skeleton/EXTRAS/Tools/miyoomini/Syncthing.pak/script/checkrun.sh
@@ -5,13 +5,9 @@ cd "$DIR"
 cd ..
 DIR="$(pwd)"
 {
-#export sysdir="/mnt/SDCARD/.tmp_update"
-#export miyoodir="/mnt/SDCARD/.system/miyoomini"
-#export LD_LIBRARY_PATH="$DIR/lib:/lib:/config/lib"
-
 if ! pgrep "syncthing" > /dev/null; then
 	echo "serving.."
     $DIR/bin/syncthing serve --home=$DIR/config/ > $DIR/serve.log 2>&1 &
 fi
-echo "test"
+
 } &> ./checkrun_log.txt
--- skeleton/EXTRAS/Tools/miyoomini/WiFi.pak/launch.sh
+++ skeleton/EXTRAS/Tools/miyoomini/WiFi.pak/launch.sh
@@ -4,9 +4,6 @@ DIR="$(dirname "$0")"
 cd "$DIR"
 {
 if $IS_PLUS; then
-#export sysdir="/mnt/SDCARD/.tmp_update"
-#export miyoodir="/mnt/SDCARD/.system/miyoomini"
-#export LD_LIBRARY_PATH="$DIR/lib:/lib:/config/lib:$miyoodir/lib"
 ./bin/st -q -e $DIR/script/launchenv.sh
 else
 	./bin/say "Only supported on Miyoo Mini Plus"$'\n'$'\n'"Press any button to exit"
